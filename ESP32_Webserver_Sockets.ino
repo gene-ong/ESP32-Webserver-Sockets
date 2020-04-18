8fab471f66e0564068938a2505fc5f86b42f99e8
Gene Ong <gene@dosec.com.au>
2020-04-18 11:08:34 +1000

    Attempting to create Access Point instead of going through router. Yet to be implemented successfully

--- ESP32_Webserver_Sockets.ino
+++ ESP32_Webserver_Sockets.ino
@@ -1,5 +1,5 @@
 #include "WiFi.h"
-#include "ESPAsyncWebServer.h"
+#include "WebSocketServer.h"
 #include "FastLED.h"
 
 #define NEO_PIN 26
@@ -8,66 +8,23 @@
 #define GRN_LED 2
 #define BLU_LED 4
 #define COL_NB 3
-const char* ssid     = "TeamEuxnix2016DS";
-const char* password = "2714thecrescent21352016";
-
-uint8_t neoPixelMatrix[NEO_NB][COL_NB] = {{0}};
-
+const char* ssid     = "MyESP32AP";
+const char* password = "testpassword";
 static int colourRGB = 0;
 CRGB leds[NEO_NB];
 
-AsyncWebServer wifiServer(80);
-AsyncWebSocket ws("/test");
+WiFiServer wifiServer(80);
+WebSocketServer webSocketServer;
 
-void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len) {
-
-  if (type == WS_EVT_CONNECT) {
-    //    Serial.println("Websocket client connection received");
-
-  } else if (type == WS_EVT_DISCONNECT) {
-    //    Serial.println("Client disconnected");
-
-  } else if (type == WS_EVT_DATA) {
-    /*
-      //DO NOT DELETE Test code to view the string coming over WEbsocket
-        Serial.print("len = ");
-        Serial.print(len);
-
-        for (int i=0; i < len; i ++ ) {
-          Serial.print(" i = ");
-          Serial.print(i);
-          Serial.print(" data = ");
-          Serial.println(data[i]);
-        }
-    */
-    for (int i = 0; i < NEO_NB; i++) {
-      leds[i].red = (uint8_t) data[i * 3 + 2];
-      leds[i].green = (uint8_t) data[i * 3 + 1];
-      leds[i].blue = (uint8_t) data[i * 3 + 0];
-    }
-    FastLED.show();
-  }
-}
 
 void setup() {
 
-    Serial.begin(115200);
+  Serial.begin(115200);
 
   delay(1000);
 
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    //    Serial.println("Connecting to WiFi..");
-  }
-
-  //  Serial.println("Connected to the WiFi network");
-  Serial.println(WiFi.localIP());
-
-  ws.onEvent(onWsEvent);
-  wifiServer.addHandler(&ws);
-
+  WiFi.softAP(ssid, password);
+  Serial.println(WiFi.softAPIP());
   wifiServer.begin();
 
   //Initialise all LED pins as outputs and off
@@ -84,4 +41,34 @@ void setup() {
 }
 
 void loop() {
+  WiFiClient client = server.available();
+
+  if (client.connected() && webSocketServer.handshake(client)) {
+
+    String data;
+
+    while (client.connected()) {
+
+      data = webSocketServer.getData();
+
+      if (data.length() > 0) {
+        Serial.println(data);
+        //webSocketServer.sendData(data);
+        for (int i = 0; i < NEO_NB; i++) {
+          leds[i].red = (uint8_t) data[i * 3 + 2];
+          leds[i].green = (uint8_t) data[i * 3 + 1];
+          leds[i].blue = (uint8_t) data[i * 3 + 0];
+        }
+        FastLED.show();
+      }
+
+      delay(10); // Delay needed for receiving the data correctly
+    }
+
+    Serial.println("The client disconnected");
+    delay(100);
+  }
+
+  delay(100);
+
 }
