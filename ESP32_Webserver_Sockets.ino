16e244604acaf5f23521dba00aafc0dc6da464a6
Gene Ong <gene@dosec.com.au>
2020-04-18 10:58:40 +1000

    Uses the HTTP posts to send values once connected to webserver. Three bytes are sent over a terminal program such as Putty or RealTerm, once connected to the IP:port. These three bytes are then utilised for controlling the value of PWM of the RGB LED on the ESP32 WROOM

--- ESP32_Webserver_Sockets.ino
+++ ESP32_Webserver_Sockets.ino
@@ -1,34 +1,4 @@
-/*
- WiFi Web Server LED Blink
-
- A simple web server that lets you blink an LED via the web.
- This sketch will print the IP address of your WiFi Shield (once connected)
- to the Serial monitor. From there, you can open that address in a web browser
- to turn on and off the LED on pin 5.
-
- If the IP address of your shield is yourAddress:
- http://yourAddress/H turns the LED on
- http://yourAddress/L turns it off
-
- This example is written for a network using WPA encryption. For
- WEP or WPA, change the Wifi.begin() call accordingly.
-
- Circuit:
- * WiFi shield attached
- * LED attached to pin 5
-
- created for arduino 25 Nov 2012
- by Tom Igoe
-
-ported for sparkfun esp32 
-31.01.2017 by Jan Hendrik Berlin
- 
- */
-
-#include <WiFi.h>
-#include <Adafruit_NeoPixel.h>
-#define NEO_PIN 26 
-#define NEO_NB 80
+#include "WiFi.h"
 #define RED_LED 0
 #define GRN_LED 2
 #define BLU_LED 4
@@ -36,94 +6,117 @@ ported for sparkfun esp32
 const char* ssid     = "TeamEuxnix2016DS";
 const char* password = "2714thecrescent21352016";
 
+// setting PWM properties
+const int freq = 5000;
+const int redLedChannel = 0;
+const int bluLedChannel = 1;
+const int grnLedChannel = 2;
+const int resolution = 8;
 
-WiFiServer server(80);
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NEO_NB, NEO_PIN, NEO_GRB + NEO_KHZ800);
-int redLEDState = 0;
-int bluLEDState = 0;
-int grnLEDState = 0;
+WiFiServer wifiServer(80);
 
-void setup()
-{
-//Init for WIFI
-    WiFi.begin(ssid, password);
+void processReceivedValue(char command) {
 
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-    }
-    server.begin();
+  static int counter;
+  Serial.print("counter = ");
+  Serial.println(counter);
+  switch (counter) {
+    case 0: ledcWrite(redLedChannel, command);
+      Serial.println(command, DEC);
+      Serial.println(command, BIN);
+      break;
+    case 1: ledcWrite(bluLedChannel, command);
+      Serial.println(command, DEC);
+      Serial.println(command, BIN);
+      break;
+    case 2: ledcWrite(grnLedChannel, command);
+      Serial.println(command, DEC);
+      Serial.println(command, BIN);
+      break;
+    default: counter = 0;
+  }
+  counter++;
+  if (counter >= 3) {
+    counter = 0;
+  }
 
-//Init for Neopixels
-  strip.begin();
-  strip.setBrightness(50);
-  strip.show(); // Initialize all pixels to 'off'
+  return;
+}
 
+void setup() {
+
+  Serial.begin(115200);
+
+  delay(1000);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  Serial.println("Connected to the WiFi network");
+  Serial.println(WiFi.localIP());
+
+  wifiServer.begin();
+
+  //Initialise all LED pins as outputs and off
   pinMode(RED_LED, OUTPUT);
   pinMode(BLU_LED, OUTPUT);
   pinMode(GRN_LED, OUTPUT);
   digitalWrite(RED_LED, LOW);
   digitalWrite(BLU_LED, LOW);
   digitalWrite(GRN_LED, LOW);
-  
+
+  // configure LED PWM functionalitites
+  ledcSetup(redLedChannel, freq, resolution);
+  ledcSetup(bluLedChannel, freq, resolution);
+  ledcSetup(grnLedChannel, freq, resolution);
+
+  // attach the channel to the GPIO to be controlled
+  ledcAttachPin(RED_LED, redLedChannel);
+  ledcAttachPin(BLU_LED, bluLedChannel);
+  ledcAttachPin(GRN_LED, grnLedChannel);
+
+
 }
 
-void loop(){
- WiFiClient client = server.available();   // listen for incoming clients
+void loop() {
 
-  if (client) {                             // if you get a client,
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        if (c == '\n') {                    // if the byte is a newline character
-          if (currentLine.length() == 0) {
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println();
-            client.print("Click <a href=\"/G\">here</a> to toggle the green LED on or off.<br>");
-            client.print("Click <a href=\"/R\">here</a> to toggle the red LED on or off.<br>");
-            client.print("Click <a href=\"/B\">here</a> to toggle the blue LED on or off.<br>");
-            client.println();
-            break;
+  // // increase the LED brightness
+  //  for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){
+  //    // changing the LED brightness with PWM
+  //    ledcWrite(redLedChannel, dutyCycle);
+  //    delay(1);
+  //  }
+  //
+  //  // decrease the LED brightness
+  //  for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--){
+  //    // changing the LED brightness with PWM
+  //    ledcWrite(redLedChannel, dutyCycle);
+  //    delay(1);
+  //  }
 
-          } else {    // if you got a newline, then clear currentLine:
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
+  WiFiClient client = wifiServer.available();
 
-        // Check to see if the client request was "GET /H" or "GET /L":
-               
-        if (currentLine.endsWith("GET /G")) {
-          colorWipe(strip.Color(0, 255, 0), 0); // Green
-          grnLEDState = !grnLEDState;
-          
+  if (client) {
+
+    while (client.connected()) {
+
+      while (client.available() > 0) {
+        char c = client.read();
+        if ((c != '\r') && (c != 10)) { // if you got anything else but a carriage return character and the decimal value of 10
+          processReceivedValue(c);
+          Serial.println(c);
         }
-        if (currentLine.endsWith("GET /R")) {
-          colorWipe(strip.Color(255, 0, 0), 0); // Red
-          redLEDState = !redLEDState;
-        }
-         if (currentLine.endsWith("GET /B")) {
-          colorWipe(strip.Color(0, 0, 255), 0); // Blue
-          bluLEDState = !bluLEDState;
-        }
-        digitalWrite(GRN_LED, grnLEDState);
-        digitalWrite(BLU_LED, bluLEDState);
-        digitalWrite(RED_LED, redLEDState);
       }
-    }
-    // close the connection:
-    client.stop();
-    //Serial.println("Client Disconnected.");
-  }
-}
 
-// Fill the dots one after the other with a color
-void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, c);
-    strip.show();
-    delay(wait);
+      delay(10);
+    }
+
+    client.stop();
+    Serial.println("Client disconnected");
+
   }
 }
