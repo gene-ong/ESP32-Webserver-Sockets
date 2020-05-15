205817fb8f7ea7528aa06d359d90d6c2da5ffab3
Gene Ong <gene@dosec.com.au>
2020-05-16 01:41:10 +1000

    Attemp to implement OctoLEDs

--- ESP32_Webserver_Sockets.ino
+++ ESP32_Webserver_Sockets.ino
@@ -1,106 +1,48 @@
 //MACROS
-#define DEVELOPMENT_LOCATION 0 //0=strathfield home,1=strathfield home 2, 2=your chemist shop Randwick
-#define WIFI_TYPE 1//0 = Local WiFi Websockets, 1 = Access Point Websockets
 #define SERIAL_PRINT 0 //0 = disable ALL serial printing, 1 = enable serial printing
 #define SERIAL_PRINT_DEBUG 0 //0 = disable excessive serial printing, enable excessive serial Printing
+#define LEDSTRING_NB 0 //0 = single LED string, otherwise value equals number of strings
 
 #include "WiFi.h"
 #include "FastLED.h"
-#if WIFI_TYPE == 0
-#include "ESPAsyncWebServer.h"
-#endif
-#if WIFI_TYPE == 1
 #include <WebSocketServer.h>
-#endif
 
+#if LEDSTRING_NB == 0
 #define NEO_PIN 26
 #define NEO_NB 80
+#else
+#define NEO_STRING_NB 208//64 x 26 matrix, divided by 8 strings = 208
+#define NEO_PIN0 21
+#define NEO_PIN1 22
+#define NEO_PIN2 19
+#define NEO_PIN3 5
+#define NEO_PIN4 17
+#define NEO_PIN5 0
+#define NEO_PIN6 2
+#define NEO_PIN7 12
+#endif
+
 #define RED_LED 0
 #define GRN_LED 2
 #define BLU_LED 4
-#define COL_NB 3
 #define WDT 2000 //watch dog timer timeout in milliseconds
 
-#if WIFI_TYPE == 0
-#if DEVELOPMENT_LOCATION == 0
-const char* ssid     = "TeamEuxnix2016DS";
-const char* password = "2714thecrescent21352016";
-#elif DEVELOPMENT_LOCATION == 1
-const char* ssid     = "10607176";
-const char* password = "capstone";
-#elif DEVELOPMENT_LOCATION == 2
-const char* ssid     = "Chemist_Shop";
-const char* password = "High_66_Randwick";
-#endif
-#elif WIFI_TYPE == 1
+
 const char *ssid = "MyESP32AP";
 const char *password = "testpassword";
+
+#if LEDSTRING_NB == 0
+CRGB leds[NEO_NB];
+#else
+CRGB leds[LEDSTRING_NB][NEO_STRING_NB];
 #endif
 
-uint8_t neoPixelMatrix[NEO_NB][COL_NB] = {{0}};
-
-static int colourRGB = 0;
-CRGB leds[NEO_NB];
 unsigned long pseudoWDT = 0; //2s watchdog timer
-#if WIFI_TYPE == 0
-AsyncWebServer wifiserver(80);
-AsyncWebSocket ws("/test");
 
-#elif WIFI_TYPE == 1
 WiFiServer wifiserver(80);
 WebSocketServer webSocketServer;
-#endif
-
-#if WIFI_TYPE == 0
-void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len) {
-
-  if (type == WS_EVT_CONNECT) {
-#if SERIAL_PRINT == 1
-    Serial.println("Websocket client connection received");
-#endif
-  } else if (type == WS_EVT_DISCONNECT) {
-#if SERIAL_PRINT == 1
-    Serial.println("Client disconnected");
-#endif
-  } else if (type == WS_EVT_DATA) {
-    /*
-      //DO NOT DELETE Test code to view the string coming over WEbsocket
-        Serial.print("len = ");
-        Serial.print(len);
-
-        for (int i=0; i < len; i ++ ) {
-          Serial.print(" i = ");
-          Serial.print(i);
-          Serial.print(" data = ");
-          Serial.println(data[i]);
-        }
-    */
-    for (int i = 0; i < NEO_NB; i++) {
-      leds[i].red = (uint8_t) data[i * 3 + 2];
-      leds[i].green = (uint8_t) data[i * 3 + 1];
-      leds[i].blue = (uint8_t) data[i * 3 + 0];
-    }
-    FastLED.show();
-#if SERIAL_PRINT == 1 && SERIAL_PRINT_DEBUG == 1
-    Serial.println(millis());
-#endif
-
-  }
-}
-#elif WIFI_TYPE == 1
-#endif
-
 
 void setup() {
-#if SERIAL_PRINT == 1
-  Serial.begin(115200);
-#endif
-  delay(1000);
-
-  //Initialise Neopixels
-  FastLED.addLeds<NEOPIXEL, NEO_PIN>(leds, NEO_NB);  // GRB ordering is assumed
-  FastLED.setBrightness( 50 );
-
 
   //Initialise all LED pins as outputs and off
   pinMode(RED_LED, OUTPUT);
@@ -110,56 +52,60 @@ void setup() {
   digitalWrite(BLU_LED, LOW);
   digitalWrite(GRN_LED, LOW);
 
-#if WIFI_TYPE == 0
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
 #if SERIAL_PRINT == 1
-    Serial.println("Connecting to WiFi..");
-    Serial.println(ssid);
-    Serial.println(password);
+  Serial.begin(115200);
+#endif
+  delay(1000);
+
+#if LEDSTRING_NB == 0
+  //Initialise Neopixels
+  FastLED.addLeds<NEOPIXEL, NEO_PIN>(leds, NEO_NB);  // GRB ordering is assumed
+  FastLED.setBrightness( 50 );
+#else
+
+  FastLED.addLeds<NEOPIXEL, NEO_PIN0>(leds[0], NEO_STRING_NB);  // GRB ordering is assumed
+  FastLED.addLeds<NEOPIXEL, NEO_PIN1>(leds[1], NEO_STRING_NB);  // GRB ordering is assumed
+  FastLED.addLeds<NEOPIXEL, NEO_PIN2>(leds[2], NEO_STRING_NB);  // GRB ordering is assumed
+  FastLED.addLeds<NEOPIXEL, NEO_PIN3>(leds[3], NEO_STRING_NB);  // GRB ordering is assumed
+  FastLED.addLeds<NEOPIXEL, NEO_PIN4>(leds[4], NEO_STRING_NB);  // GRB ordering is assumed
+  FastLED.addLeds<NEOPIXEL, NEO_PIN5>(leds[5], NEO_STRING_NB);  // GRB ordering is assumed
+  FastLED.addLeds<NEOPIXEL, NEO_PIN6>(leds[6], NEO_STRING_NB);  // GRB ordering is assumed
+  FastLED.addLeds<NEOPIXEL, NEO_PIN7>(leds[7], NEO_STRING_NB);  // GRB ordering is assumed
+  FastLED.addLeds<NEOPIXEL, NEO_PIN1>(leds[0], NEO_STRING_NB);  // GRB ordering is assumed
+  FastLED.setBrightness( 50 );
 #endif
 
-  }
-#if SERIAL_PRINT == 1
-  Serial.println("Connected to the WiFi network");
-  Serial.println(WiFi.localIP());
-#endif
-  ws.onEvent(onWsEvent);
-  wifiserver.addHandler(&ws);
-  wifiserver.begin();
 
-#elif WIFI_TYPE == 1
+
+#if LEDSTRING_NB == 0
   //Initialise a red LED to indicate we are connecting
   leds[0].red = 255;
   leds[0].green = 0;
   leds[0].blue = 0;
   FastLED.show();
+#endif
 
   WiFi.softAP(ssid, password);
 
+#if LEDSTRING_NB == 0
   //Once connected change to a blue LED
   leds[0].red = 0;
   leds[0].green = 0;
   leds[0].blue = 255;
   FastLED.show();
+#endif
 
 #if SERIAL_PRINT == 1
   Serial.println(WiFi.softAPIP());
 #endif
-#endif
-
-
 
 
 }
 
 void loop() {
-#if WIFI_TYPE == 1
   static int firstConnect = 0;
-
   wifiserver.begin();
+  
 #if SERIAL_PRINT == 1
   Serial.println("WebServer begin");
 #endif
@@ -186,12 +132,21 @@ void loop() {
         //        for (int i = 0; i < NEO_NB; i++) {
         //          Serial.print((uint8_t)data[i]);
         //          Serial.print(" ");
-
+#if LEDSTRING_NB == 0
         for (int i = 0; i < NEO_NB; i++) {
           leds[i].red = (uint8_t) data[i * 3 + 2];
           leds[i].green = (uint8_t) data[i * 3 + 1];
           leds[i].blue = (uint8_t) data[i * 3 + 0];
         }
+#else
+        for (int i = 0; i < LEDSTRING_NB; i++) {
+          for (int x = 0; x < NEO_STRING_NB; x++) {
+            leds[i][x].red = (uint8_t) data[i * 3 + 2];
+            leds[i][x].green = (uint8_t) data[i * 3 + 1];
+            leds[i][x].blue = (uint8_t) data[i * 3 + 0];
+          }
+        }
+#endif
         FastLED.show();
         //        }
 #if SERIAL_PRINT == 1 && SERIAL_PRINT_DEBUG == 1
@@ -242,5 +197,4 @@ void loop() {
     ESP.restart();
 
   }
-#endif
 }
